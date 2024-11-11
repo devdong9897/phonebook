db6666441a7fae8370be290dec16a3dc516d7543
devdong9897 <dev.dong9897@gmail.com>
2024-11-11 11:15:34 +0900

    전화번호부 왼쪽쪽UI작업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import ContactForm from "./components/ContactForm";
 
+// 1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search창이 있다.
+// 2. 리스트에 유저이름과 전화번호를 추가할 수 있다.
+// 3. 리스트에 아이템이 몇개 있는지 보인다.
+// 4. 사용자가 유저를 이름검색으로 찾을 수 있다.
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="title">연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
